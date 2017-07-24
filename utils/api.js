ec60730980fcc5454b44cd33138b7c5ccab7fe39
lujianan <ljn6176@163.com>
2017-07-24 21:02:54 +0800

    wechat-cnode

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const HOST_URI = 'https://cnodejs.org/api/v1';
+
+module.exports = {
+  // 获取列表
+  getTopics: (obj) => {
+
+  },
+
+  // 获取详细内容
+  getTopicDetail: () => {
+
+  }
+};
